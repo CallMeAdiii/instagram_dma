aea6eb84afdc1e134c88cbf8855c6c4436b16cfe
CallMeAdiii <ondrejbrazdil1@gmail.com>
2025-10-17 12:22:07 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <title>Mini Instagram</title>
+    <link rel="stylesheet" href="style.css">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+</head>
+<body>
+
+
+
+<?php
+
+$posts = [
+
+
+
+
+
+]
