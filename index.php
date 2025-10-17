6918899e41e639b0b119c63e3707e540ccb60870
CallMeAdiii <ondrejbrazdil1@gmail.com>
2025-10-17 12:43:09 +0200

    Associative array

--- index.php
+++ index.php
@@ -12,11 +12,46 @@
 
 
 <?php
-
+// Asociativní pole příspěvků, Zde jsem si vypsal všechny věci které se budou zobrazovat
 $posts = [
 
+    [
+        "username" => "ondra_dev",
+        "profile_pic" => "https://i.pravatar.cc/50?img=1", // profilovka
+        "image" => "https://picsum.photos/600/400?random=1",
+        "likes" => 128,
+        "caption" => "Dneska chill coding session ☕💻",
+        "time" => "2 Hours ago",
+        "comments" => [
+            "username" => "code_master",
+            "text" => "Nice setup bro! 🔥"
+        ]
+    ],
 
 
+    [
+        "username" => "nature_vibec",
+        "profile_pic" => "https://i.pravatar.cc/50?img=2",
+        "image" => "https://picsum.photos/600/400?random=2",
+        "likes" => 542,
+        "caption" => "Pohled, který nikdy neomrzí  🌲🌄",
+        "time" => "5 Hours ago",
+        "comments" => [
+            "username" => "nature_lover",
+            "text" => "Nice view 🔥"
+        ]
+    ],
+    [
+        "username" => "gym_grind",
+        "profile_pic" => "https://i.pravatar.cc/50?img=3",
+        "image" => "https://picsum.photos/600/400?random=3",
+        "likes" => 512,
+        "caption" => "No pain, no gain 💪🔥",
+        "time" => "1 day ago",
+        "comments" => [
+            "username" => "fitness_trainer",
+            "text" => "Respect! 🔥💪"
+        ]
+    ],
 
-
-]
+    ];
